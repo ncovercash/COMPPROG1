3621e76abd6b61a0486b197aa00f1b585175b95f
Noah Overcash <smileytechguy@smileytechguy.com>
2015-10-12 13:38:44 -0400

    Worked on 016

--- Ch04/A3_016_ThreeButtonRoll_Overcash.java
+++ Ch04/A3_016_ThreeButtonRoll_Overcash.java
@@ -3,20 +3,28 @@ import java.awt.*;
 import java.util.*;
 public class A3_016_ThreeButtonRoll_Overcash extends WindowController {
     RandomIntGenerator rGen = new RandomIntGenerator(1, 3);
-    int redColor;
-	int blueColor;
-	int greenColor;
+    int redColor = 3;
+	int blueColor = 2;
+	int greenColor = 1;
 	int offSet;
 	int oneThirdScreenX;
 	int twoThirdScreenX;
 	int boxYValue;
 	int boxWidth;
 	int boxHeight;
-	int pointsThisRound;
-	int totalScore;
-	int changeColor;
+	int pointsThisRound = 0;
+	int totalScore = 0;
+	int changeColor = 0;
     public static void main(String[] args) {
         A3_016_ThreeButtonRoll_Overcash class = new A3_016_ThreeButtonRoll_Overcash();
+    }
+    public void begin() {
+		offSet = canvas.getWidth()*0.03;
+		oneThirdScreenX = canvas.getWidth()/3;
+		twoThirdScreenX = canvas.getWidth()/1.5;
+		boxYValue = canvas.getHeight()/3+offSet;
+		boxWidth = canvas.getWidth()*0.3;
+		boxHeight = canvas.getHeight()*0.3;
     }
     public void onMouseClick(Location p) {
     	// do stuff
