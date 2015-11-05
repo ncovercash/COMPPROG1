66833fd71437c30e576ca77b3a3c8340b19fb1e2
Noah Overcash <smileytechguy@smileytechguy.com>
2015-11-05 12:36:16 -0500

    init Holiday Driver

---
+++ Holiday/Driver.java
@@ -0,0 +1,15 @@
+import objectdraw.*;
+import java.awt.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import javax.imageio.*;
+public class Driver extends WindowController {
+	public static void main(String[] args) {
+		Driver oc = new Driver();
+		oc.startController(1000, 1000);
+	}
+	public void onMouseClick(Location p) {
+		
+	}
+}
