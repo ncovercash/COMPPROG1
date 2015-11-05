2854bb2ce9701145888da715f4a0453001e649e2
Noah Overcash <smileytechguy@smileytechguy.com>
2015-11-05 13:14:34 -0500

    Finished Driver

--- Holiday/Driver.java
+++ Holiday/Driver.java
@@ -15,8 +15,11 @@ public class Driver extends WindowController {
 	}
 	public void drawGift(int w, int h, Location p) {
 		base = new FilledRect(p, w, h, canvas);
-		base.setColor(giftBorder);
-		new FilledRect(base.getX()+base.getWidth()/20, base.getY()+base.getHeight()/20, 10, 10, canvas);
+		base.setColor(giftBorderColor);
+		new FilledRect(base.getX()+base.getWidth()/20, base.getY()+base.getHeight()/20, base.getWidth()-base.getWidth()/10, base.getHeight()-base.getHeight()/10, canvas).setColor(giftInsideColor);;
+		new FilledRect(base.getX()+base.getWidth()/2-base.getWidth()/10, base.getY(), base.getWidth()/5, base.getHeight(), canvas).setColor(giftRibbonColor);
+		new FilledRect(base.getX(), base.getY()+base.getHeight()/2-base.getHeight()/10, base.getWidth(), base.getHeight()/5, canvas).setColor(giftRibbonColor);
+		new FilledOval(base.getX()+base.getWidth()/2-base.getWidth()/6, base.getY()+base.getHeight()/2-base.getHeight()/6, base.getWidth()/3, base.getHeight()/3, canvas).setColor(giftRibbonColor);
 	}
 	public void onMouseClick(Location p) {
 		drawGift(100, 100, p);
