e93b47c42015d5ed48c7351469437ffe1259af77
Noah Overcash <noah@Noahs-MBP.local>
2016-04-19 08:11:16 -0400

    SI fix

--- Cacalatur/Cacalatur.java
+++ Cacalatur/Cacalatur.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.math.*;
 import javax.imageio.*;
 public class Cacalatur extends ActiveObject {
 	DrawingCanvas c;
@@ -94,21 +95,24 @@ public class Cacalatur extends ActiveObject {
 						break;
 					case 6:
 						val1 = top.getScore();
-						top.setScore(1/top.getScore());
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(1/top.getScore() * 10000d) / 10000d);
 						tapeEntries.add("1 / "+val1+" = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 7:
 						val1 = top.getScore();
-						top.setScore(Math.pow(top.getScore(), 2));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.pow(top.getScore(), 2) * 10000d) / 10000d);
 						tapeEntries.add(val1+"² = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 8:
 						val1 = top.getScore();
-						top.setScore(Math.sqrt(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.sqrt(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("√("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
@@ -125,7 +129,7 @@ public class Cacalatur extends ActiveObject {
 						break;
 					case 10:
 						double result = Math.random();
-						top.setScore(result);
+						top.setScore((double)Math.round(result * 10000d) / 10000d);
 						tapeEntries.add("rand() = "+result);
 						updateTape();
 						inputs = new ArrayList<String>();
@@ -139,107 +143,128 @@ public class Cacalatur extends ActiveObject {
 						break;
 					case 12:
 						val1 = top.getScore();
-						top.setScore(Math.floor(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.floor(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("floor("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 13:
 						val1 = top.getScore();
-						top.setScore(Math.ceil(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.ceil(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("ceil("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 14:
 						val1 = top.getScore();
-						top.setScore(top.getScore()+1);
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(top.getScore()+1 * 10000d) / 10000d);
 						tapeEntries.add(val1+"++ = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 15:
 						val1 = top.getScore();
-						top.setScore(top.getScore()-1);
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(top.getScore()-1 * 10000d) / 10000d);
 						tapeEntries.add(val1+"-- = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 16:
 						val1 = top.getScore();
-						top.setScore(Math.round(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.round(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("round("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 17:
 						val1 = top.getScore();
-						top.setScore(Math.pow(Math.E, top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.pow(Math.E, top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("e^"+val1+" = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 18:
 						val1 = top.getScore();
-						top.setScore(Math.sin(Math.toRadians(val1)));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.sin(Math.toRadians(val1)) * 10000d) / 10000d);
 						tapeEntries.add("sin("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 19:
 						val1 = top.getScore();
-						top.setScore(Math.cos(Math.toRadians(val1)));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.cos(Math.toRadians(val1)) * 10000d) / 10000d);
 						tapeEntries.add("cos("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 20:
 						val1 = top.getScore();
-						top.setScore(Math.tan(Math.toRadians(val1)));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.tan(Math.toRadians(val1)) * 10000d) / 10000d);
 						tapeEntries.add("tan("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 21:
-						mode.setText("SI");
+						// mode.setText("SCI");
+						val1 = top.getScore();
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						tapeEntries.add(val1+" => "+getSIs(val1));
+						updateTape();
 						break;
 					case 22:
 						mode.setText("DEG");
 						break;
 					case 23:
-						mode.setText("SCI");
+						// mode.setText("SCI");
+						val1 = top.getScore();
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						tapeEntries.add(val1+" => "+parseToCientificNotation(val1));
+						updateTape();
 						break;
 					case 24:
 						val1 = top.getScore();
-						top.setScore(Math.sin(Math.toRadians(val1)));
-						top.setScore(1/(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.sin(Math.toRadians(val1)) * 10000d) / 10000d);
+						top.setScore((double)Math.round(1/(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("sin⁻¹("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 25:
 						val1 = top.getScore();
-						top.setScore(Math.cos(Math.toRadians(val1)));
-						top.setScore(1/(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.cos(Math.toRadians(val1)) * 10000d) / 10000d);
+						top.setScore((double)Math.round(1/(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("cos⁻¹("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 26:
 						val1 = top.getScore();
-						top.setScore(Math.tan(Math.toRadians(val1)));
-						top.setScore(1/(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.tan(Math.toRadians(val1)) * 10000d) / 10000d);
+						top.setScore((double)Math.round(1/(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("tan⁻¹("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 27:
 						inputs = new ArrayList<String>();
-						top.setScore(Math.E);
+						top.setScore((double)Math.round(Math.E * 10000d) / 10000d);
 						break;
 					case 28:
 						val1 = top.getScore();
-						top.setScore(Math.pow(10, top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.pow(10, top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("10^"+val1+" = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
@@ -247,6 +272,7 @@ public class Cacalatur extends ActiveObject {
 					case 29:
 						if (calculateInput() != top.getScore()) {
 							val1 = top.getScore();
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
 						} else {
 							updateDisplay();
 							val1 = calculateInput();
@@ -257,14 +283,16 @@ public class Cacalatur extends ActiveObject {
 						break;
 					case 30:
 						val1 = top.getScore();
-						top.setScore(Math.log10(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.log10(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("log("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 31:
 						val1 = top.getScore();
-						top.setScore(Math.log(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.log(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("ln("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
@@ -274,21 +302,23 @@ public class Cacalatur extends ActiveObject {
 					case 33:
 						break;
 					case 34:
+						break;
+					case 35:
 						if (calculateInput() != top.getScore()) {
 							val1 = top.getScore();
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
 						} else {
 							updateDisplay();
 							val1 = calculateInput();
 						}
-						op=34;
+						op=35;
 						// updateTape();
 						inputs = new ArrayList<String>();
-						break;
-					case 35:
 						break;
 					case 36:
 						if (calculateInput() != top.getScore()) {
 							val1 = top.getScore();
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
 						} else {
 							updateDisplay();
 							val1 = calculateInput();
@@ -299,7 +329,7 @@ public class Cacalatur extends ActiveObject {
 						break;
 					case 37:
 						inputs = new ArrayList<String>();
-						top.setScore(Math.E);
+						top.setScore((double)Math.round(Math.E * 10000d) / 10000d);
 						break;
 					case 38:
 						inputs.add("7");
@@ -316,6 +346,7 @@ public class Cacalatur extends ActiveObject {
 					case 41:
 						if (calculateInput() != top.getScore()) {
 							val1 = top.getScore();
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
 						} else {
 							updateDisplay();
 							val1 = calculateInput();
@@ -326,14 +357,16 @@ public class Cacalatur extends ActiveObject {
 						break;
 					case 42:
 						val1 = top.getScore();
-						top.setScore(Math.pow(top.getScore(), 3));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.pow(top.getScore(), 3) * 10000d) / 10000d);
 						tapeEntries.add(val1+"³ = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
 						break;
 					case 43:
 						val1 = top.getScore();
-						top.setScore(Math.cbrt(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.cbrt(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("∛("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
@@ -353,6 +386,7 @@ public class Cacalatur extends ActiveObject {
 					case 47:
 						if (calculateInput() != top.getScore()) {
 							val1 = top.getScore();
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
 						} else {
 							updateDisplay();
 							val1 = calculateInput();
@@ -363,7 +397,8 @@ public class Cacalatur extends ActiveObject {
 						break;
 					case 48:
 						val1 = top.getScore();
-						top.setScore(Math.abs(top.getScore()));
+						val1 = (double)Math.round(val1 * 10000d) / 10000d;
+						top.setScore((double)Math.round(Math.abs(top.getScore()) * 10000d) / 10000d);
 						tapeEntries.add("abs("+val1+") = "+top.getScore());
 						updateTape();
 						inputs = new ArrayList<String>();
@@ -371,6 +406,7 @@ public class Cacalatur extends ActiveObject {
 					case 49:
 						if (calculateInput() != top.getScore()) {
 							val1 = top.getScore();
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
 						} else {
 							updateDisplay();
 							val1 = calculateInput();
@@ -401,42 +437,49 @@ public class Cacalatur extends ActiveObject {
 									result = calculateInput();
 								} else {
 									val1 = top.getScore();
+									val1 = (double)Math.round(val1 * 10000d) / 10000d;
 									result = top.getScore();
 								}
 								tapeEntries.add(val1 + " = " + result);
 								break;
 							case 29:
 								val2 = calculateInput();
+								val2 = (double)Math.round(val2 * 10000d) / 10000d;
 								result = val1/val2;
 								tapeEntries.add(val1 + " ÷ " + val2 + " = " + result);
 								break;
 							case 35:
 								val2 = calculateInput();
+								val2 = (double)Math.round(val2 * 10000d) / 10000d;
 								result = val1*val2;
 								tapeEntries.add(val1 + " * " + val2 + " = " + result);
 								break;
 							case 36:
 								val2 = calculateInput();
+								val2 = (double)Math.round(val2 * 10000d) / 10000d;
 								result = Math.pow(val1, val2);
 								tapeEntries.add(val1 + "^" + val2 + " = " + result);
 								break;
 							case 41:
 								val2 = calculateInput();
+								val2 = (double)Math.round(val2 * 10000d) / 10000d;
 								result = val1-val2;
 								tapeEntries.add(val1 + " - " + val2 + " = " + result);
 								break;
 							case 47:
 								val2 = calculateInput();
+								val2 = (double)Math.round(val2 * 10000d) / 10000d;
 								result = val1+val2;
 								tapeEntries.add(val1 + " + " + val2 + " = " + result);
 								break;
 							case 49:
 								val2 = calculateInput();
+								val2 = (double)Math.round(val2 * 10000d) / 10000d;
 								result = val1%val2;
 								tapeEntries.add(val1 + " % " + val2 + " = " + result);
 								break;
 						}
-						top.setScore(result);
+						top.setScore((double)Math.round(result * 10000d) / 10000d);
 						updateTape();
 						val2 = 0;
 						op = -1;
@@ -448,13 +491,15 @@ public class Cacalatur extends ActiveObject {
 					case 55:
 						if ((int)top.getScore() == top.getScore()) {
 							val1 = top.getScore();
-							top.setScore(1);
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
+							top.setScore((double)Math.round(1 * 10000d) / 10000d);
 							tapeEntries.add("isint("+val1+") = YES");
 							updateTape();
 							inputs = new ArrayList<String>();
 						} else {
 							val1 = top.getScore();
-							top.setScore(0);
+							val1 = (double)Math.round(val1 * 10000d) / 10000d;
+							top.setScore((double)Math.round(0 * 10000d) / 10000d);
 							tapeEntries.add("isint("+val1+") = NO");
 							updateTape();
 							inputs = new ArrayList<String>();
@@ -545,8 +590,14 @@ public class Cacalatur extends ActiveObject {
 			return 0;
 		}
 	}
+	public int getSIs(double in) {
+		BigDecimal input = new BigDecimal(in);
+  		return input.scale() <= 0
+        ? input.precision() + input.stripTrailingZeros().scale()
+        : input.precision();
+	}
 	public void updateDisplay() {
-		top.setScore(calculateInput());
+		top.setScore((double)Math.round(calculateInput() * 10000d) / 10000d);
 	}
 	public Button createButton(int row, int col, int totalRows, int totalCols, String string, int opID) {
 		if (string == "=") {
@@ -554,5 +605,19 @@ public class Cacalatur extends ActiveObject {
 		} else {
 			return new Button(string, lowerSecBorder.getX()+6+((lowerSecBorder.getWidth()-8)/totalCols*col), lowerSecBorder.getY()+(2*row)+(((lowerSecBorder.getHeight()-12)/totalRows)*row), (lowerSecBorder.getWidth()-8)/totalCols-4, (lowerSecBorder.getHeight()-(4*totalRows)-8)/totalRows, new Color(0xffffff), new Color(0x000000), c, Integer.parseInt(""+row+col), opID);
 		}
+	}
+	public static String parseToCientificNotation(double value) {
+		int cont = 0;
+		java.text.DecimalFormat DECIMAL_FORMATER = new java.text.DecimalFormat("0.####");
+		while (value/10 > 1 || value*10 < 10) {
+			if (value/10 > 1) {
+				value /= 10;
+				cont++;
+			} else {
+				value *= 10;
+				cont--;
+			}
+		}
+		return DECIMAL_FORMATER.format(value).replace(",", ".") + " x10^" + cont;
 	}
 }
